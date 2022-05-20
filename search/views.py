a4d0807c28aff4d9738c3317761595a05491ea45
BlackWizlock <aa.salikov@mail.ru>
2022-05-20 16:01:23 +0300

    Ver. 220520

--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from utils.utils import search_for_posts
 
 search_blueprint = Blueprint("search", __name__, template_folder="templates")
@@ -6,4 +6,15 @@ search_blueprint = Blueprint("search", __name__, template_folder="templates")
 
 @search_blueprint.route("/")
 def search_page():
+	s = request.args.get("s")
+	if s:
+		return render_template("search_result.html", search_request=search_for_posts(s))
 	return render_template("search.html")
+
+
+
+# @search_blueprint.route("/")
+# def search_result():
+# 	s = request.args["s"]
+# 	#return render_template("search_result.html", search_request=s)
+# 	return "WOW"
