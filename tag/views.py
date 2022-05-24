bcaf8f2c3ffcb19ab1b03e0aca524b76a00c0463
BlackWizlock <aa.salikov@mail.ru>
2022-05-24 10:43:29 +0300

    Ver. 220524

--- tag/views.py
+++ tag/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
+from utils.utils import search_for_posts
 
 tag_blueprint = Blueprint("tag", __name__, template_folder="templates")
 
 
-@tag_blueprint.route("/")
-def tag_page():
-	return render_template("tag.html")
+@tag_blueprint.route("/<string:tag>")
+def tag_page(tag: str):
+	return render_template("tag.html", search_request=search_for_posts(tag), tag=tag)
--- utils/utils.py
+++ utils/utils.py
@@ -6,93 +6,93 @@ URL_TO_DATA = "data/data.json"
 URL_TO_COMMENTS = "data/comments.json"
 
 
-def get_comments_all(url: str = URL_TO_COMMENTS):
-    """
-    Возвращает комментарии - RAW база.
-    :param url ссылка на БД
-    """
-    try:
-        with open(url, "r", encoding="UTF-8") as f:
-            return json.load(f)
-    except JSONDecodeError:
-        return abort(400)
-    except FileNotFoundError:
-        return abort(400)
+def get_comments_all(url: str = URL_TO_COMMENTS) -> any:
+	"""
+	Возвращает комментарии - RAW база.
+	:param url ссылка на БД
+	"""
+	try:
+		with open(url, "r", encoding="UTF-8") as f:
+			return json.load(f)
+	except JSONDecodeError:
+		return abort(400)
+	except FileNotFoundError:
+		return abort(400)
 
 
-def get_posts_all(url: str = URL_TO_DATA):
-    """
-    Возвращает посты - RAW база.
-    :param url ссылка на БД
-    """
-    try:
-        with open(url, "r", encoding="UTF-8") as f:
-            return json.load(f)
-    except JSONDecodeError:
-        return abort(400)
-    except FileNotFoundError:
-        return abort(400)
+def get_posts_all(url: str = URL_TO_DATA) -> any:
+	"""
+	Возвращает посты - RAW база.
+	:param url ссылка на БД
+	"""
+	try:
+		with open(url, "r", encoding="UTF-8") as f:
+			return json.load(f)
+	except JSONDecodeError:
+		return abort(400)
+	except FileNotFoundError:
+		return abort(400)
 
 
-def get_posts_by_user(user_name: str, database: list = get_posts_all()):
-    """
-    Возвращает посты определенного пользователя.
-    Функция должна вызывать ошибку `ValueError` если такого пользователя нет
-    и пустой список, если у пользователя нет постов.
-    """
-    output_data = []
-    user_not_in_database = True
-    for line in database:
-        if line["poster_name"] == user_name:
-            output_data.append(line)
-            user_not_in_database = False
-    if user_not_in_database:
-        # raise ValueError() # По курсовой тут райз - он останавливает сервер, реализовал через abort на 404 Page Not Found
-        abort(404)
-    return output_data
+def get_posts_by_user(user_name: str, database: list = get_posts_all()) -> any:
+	"""
+	Возвращает посты определенного пользователя.
+	Функция должна вызывать ошибку `ValueError` если такого пользователя нет
+	и пустой список, если у пользователя нет постов.
+	"""
+	output_data = []
+	user_not_in_database = True
+	for line in database:
+		if line["poster_name"] == user_name:
+			output_data.append(line)
+			user_not_in_database = False
+	if user_not_in_database:
+		# raise ValueError() # По курсовой тут райз - он останавливает сервер, реализовал через abort на 404 Page Not Found
+		abort(404)
+	return output_data
 
 
-def get_comments_by_post_id(post_id: int, database: list = get_comments_all()):
-    """
-    Возвращает комментарии определенного поста.
-    Функция должна вызывать ошибку `ValueError` если такого поста нет
-    и пустой список, если у поста нет комментов.
-    """
-    output_data = []
-    post_id_not_in_database = True
-    for line in database:
-        if line["post_id"] == post_id:
-            output_data.append(line)
-            post_id_not_in_database = False
-    if post_id_not_in_database:
-        # raise ValueError() # По курсовой тут райз - он останавливает сервер, реализовал через abort на 404 Page Not Found
-        abort(404)
-    return output_data
+def get_comments_by_post_id(post_id: int, database: list = get_comments_all()) -> any:
+	"""
+	Возвращает комментарии определенного поста.
+	Функция должна вызывать ошибку `ValueError` если такого поста нет
+	и пустой список, если у поста нет комментов.
+	"""
+	output_data = []
+	post_id_not_in_database = True
+	for line in database:
+		if line["post_id"] == post_id:
+			output_data.append(line)
+			post_id_not_in_database = False
+	if post_id_not_in_database:
+		# raise ValueError() # По курсовой тут райз - он останавливает сервер, реализовал через abort на 404 Page Not Found
+		abort(404)
+	return output_data
 
 
-def search_for_posts(query: str, database: list = get_posts_all()):
-    """
-    Возвращает список постов по ключевому слову.
-    """
-    output_data = []
-    for line in database:
-        if query.lower() in findall(r"\w+", line["content"].lower()):
-            output_data.append(line)
-    return output_data
+def search_for_posts(query: str, database: list = get_posts_all()) -> list:
+	"""
+	Возвращает список постов по ключевому слову.
+	"""
+	output_data = []
+	for line in database:
+		if query.lower() in findall(r"\w+", line["content"].lower()):
+			output_data.append(line)
+	return output_data
 
 
-def get_post_by_pk(pk: int, database: list = get_posts_all()):
-    """
-    Возвращает один пост по его идентификатору.
-    """
-    for line in database:
-        if line["pk"] == pk:
-            return line
-    raise abort(400)
+def get_post_by_pk(pk: int, database: list = get_posts_all()) -> any:
+	"""
+	Возвращает один пост по его идентификатору.
+	"""
+	for line in database:
+		if line["pk"] == pk:
+			return line
+	raise abort(400)
 
 
 def re_subbing(content: str) -> str:
-    hashtags = findall(r"#\w+\s", content)
-    for item in hashtags:
-        content = sub(item, f'<a href="/tag/{item[1:-1]}" class="item__tag">{item}</a>', content)
-    return content
+	hashtags = findall(r"#\w+\s", content)
+	for item in hashtags:
+		content = sub(item, f'<a href="/tag/{item[1:-1]}" class="item__tag">{item}</a>', content)
+	return content
